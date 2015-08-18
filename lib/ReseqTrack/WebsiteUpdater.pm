5b4511d0780e90ab686119b7725ac356746aac0b
gca <streeter@ebi.ac.uk>
2015-08-18 17:32:27 +0100

    WebsiteUpdater to use the config file

--- lib/ReseqTrack/WebsiteUpdater.pm
+++ lib/ReseqTrack/WebsiteUpdater.pm
@@ -4,6 +4,8 @@ use Mojo::Base 'Mojolicious';
 sub startup {
     my ($self) = @_;
 
+    $self->plugin('Config', file => $self->home->rel_file('config/website_updater.conf'));
+
     $self->routes->get('/update_project/:project')->to(controller => 'websiteupdater', action=> 'update_project');
 
 }
