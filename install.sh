27a034fb085b6653d23f7ab49496e4d5ad9e27e5
gca <streeter@ebi.ac.uk>
2015-08-18 23:31:06 +0100

    install git locally

--- install.sh
+++ install.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
-cpanm -L local git@github.com:Ensembl/ensembl-git-tools.git
-carton install
+WU_HOME=/nfs/public/rw/reseq-info/static_website_updater/gca_website_updater
+
+mkdir -p $WU_HOME/local/
+
+#cpanm -L local git@github.com:Ensembl/ensembl-git-tools.git \
+cpanm -L local git@github.com:istreeter/ensembl-git-tools.git \
+&& carton install --deployment \
+&& curl https://codeload.github.com/git/git/tar.gz/v2.5.0 >git.tar.gz \
+&& tar -xzf git.tar.gz \
+&& cd git-2.5.0/ \
+&&  make prefix=$WU_HOME/local/ \
+&&  make prefix=$WU_HOME/local/
+
+rm -fr git.tar.gz git-2.5.0
