6e7b3293ca3c38aafb7079529a69a24269091a6a
istreeter <streeter@ebi.ac.uk>
2015-08-19 16:18:57 +0100

    function to delay the update if one is already running

--- lib/ReseqTrack/WebsiteUpdater/Controller/WebsiteUpdater.pm
+++ lib/ReseqTrack/WebsiteUpdater/Controller/WebsiteUpdater.pm
@@ -1,36 +1,82 @@
 package ReseqTrack::WebsiteUpdater::Controller::WebsiteUpdater;
 use Mojo::Base 'Mojolicious::Controller';
+use Mojo::IOLoop;
 use EnsEMBL::Git;
 use File::Rsync;
 
 sub update_project {
   my ($self) = @_;
 
-  $self->inactivity_timeout(120);
+  my $project = $self->stash('project');
+  my $project_config = $self->config('projects')->{$project};
+  $self->not_found("no project config for $project") if !$project_config;
+
+  my $log_dir = $self->stash('updating_log_dir') || $self->app->home->rel_dir('var/run');
+  my $updating_limiter = $self->stash('updating_limiter') || 120;
+
+  my $log_file = "$log_dir/$project.log";
+  my $current_time = time();
+  if (-f $log_file) {
+    open my $fh, '<', $log_file or return $self->server_error("could not open $log_file $!");
+    my $line = <$fh>;
+    close $fh;
+    if ($line && $line =~ /queue (\d+)/) {
+      my $queue_time = $1;
+      my $time_to_sleep = $queue_time + $updating_limiter - $current_time;
+      return $self->render(text => "update will run in $time_to_sleep seconds");
+    }
+    if ($line && $line =~ /running (\d+)/) {
+      my $running_time = $1;
+      my $time_to_sleep = $running_time + $updating_limiter - $current_time;
+      if ($time_to_sleep > 0) {
+        $self->render(text => "update will run in $time_to_sleep seconds");
+        open my $fh, '>', $log_file or return $self->server_error("could not open $log_file $!");
+        print $fh "queue $running_time\n";
+        close $fh;
+        Mojo::IOLoop->timer($time_to_sleep => sub {
+          $current_time = time();
+          open my $fh, '>', $log_file or return $self->app->log->info("could not open $log_file $!");
+          print $fh "running $current_time\n";
+          close $fh;
+          $self->update_project_now();
+        });
+        return;
+      }
+    }
+  }
+
+  open my $fh, '>', $log_file or return $self->server_error("could not open $log_file $!");
+  print $fh "running $current_time\n";
+  close $fh;
+  $self->render(text => "update will run now");
+  $self->update_project_now();
+}
+
+
+sub update_project_now {
+  my ($self) = @_;
+
   $self->fork_call( sub {
     my ($self) = @_;
-
     my $project = $self->stash('project');
     my $project_config = $self->config('projects')->{$project};
-    return(\&not_found, "no project config for $project") if !$project_config;
-
     my $git_branch = $project_config->{branch} || 'master';
     my $git_remote = $project_config->{remote} || 'origin';
 
-    my $dir = $project_config->{'git_directory'} or return (\&server_error, "no git_directory for $project");
-    chdir $dir or return (\&server_error, "could not change to $dir");
+    my $dir = $project_config->{'git_directory'} or return ("no git_directory for $project");
+    chdir $dir or return ("could not change to $dir");
 
 
-    is_git_repo() or return (\&server_error, "$dir is not a git repo");
-    is_tree_clean() or return (\&server_error, "$dir is not clean");
+    is_git_repo() or return ("$dir is not a git repo");
+    is_tree_clean() or return ("$dir is not clean");
 
     my $start_branch = current_branch();
     if (!$start_branch || $start_branch eq 'HEAD') {
-      return (\&server_error, "HEAD is currently not on any branch. If you are in middle of a rebase or merge, please fix or abort it before continuing.");
+      return ("HEAD is currently not on any branch. If you are in middle of a rebase or merge, please fix or abort it before continuing.");
     }
 
     fetch();
-    checkout_tracking($git_branch, $git_remote) or return (\&server_error, "could not checkout $git_branch on $git_remote $dir");
+    checkout_tracking($git_branch, $git_remote) or return ("could not checkout $git_branch on $git_remote $dir");
 
     sub reset_and_return {
       my (@returns) = @_;
@@ -42,30 +88,29 @@ sub update_project {
       # local is behind or diverged (can_fastforward_merge return true if local branch is ahead of remote)
       if (!ff_merge("$git_remote/$git_branch")) {
         # this will fail if branches are diverged
-          reset_and_return(\&server_error, "Branch '$git_branch' is diverged from remote. Please do a `git pull --rebase` on '$git_branch' before continuing.");
+          reset_and_return("Branch '$git_branch' is diverged from remote. Please do a `git pull --rebase` on '$git_branch' before continuing.");
       }
     }
 
     system('bundle exec jekyll build');
-    reset_and_return(\&server_error, "failed to build jekyll: $!") if $?;
+    reset_and_return("failed to build jekyll: $!") if $?;
     if (my $signal = $? & 127) {
-      reset_and_return(\&server_error, "jekyll exited with with $signal");
+      reset_and_return("jekyll exited with with $signal");
     }
     if (my $exit = $? >>8) {
-      reset_and_return(\&server_error, "jekyll exited with code $exit");
+      reset_and_return("jekyll exited with code $exit");
     }
 
     my $rsync = File::Rsync->new(archive => 1, compress => 1, 'delete-after' => 1);
     foreach my $dest (@{$project_config->{rsync_dests}}) {
-      $rsync->exec(src => "$dir/_site/", dest => $dest) or reset_and_return(\&server_error, "could not rsync $dir/_site/ to $dest ". scalar $rsync->lastcmd);
+      $rsync->exec(src => "$dir/_site/", dest => $dest) or reset_and_return("could not rsync $dir/_site/ to $dest ". scalar $rsync->lastcmd);
     }
     reset_and_return();;
   },
   [$self],
   sub {
-    my ($self, $handler, @handler_args) = @_;
-    return $handler->($self, @handler_args) if defined $handler;
-    $self->render(text=>"success\n");
+    my ($self, @handler_args) = @_;
+    $self->app->log->info(@handler_args);
   });
 
 
