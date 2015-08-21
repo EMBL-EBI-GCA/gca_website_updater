fd1f527301cc5b492d6be6d049d11a08158e803d
gca <streeter@ebi.ac.uk>
2015-08-21 23:56:33 +0100

    no longer checking json for errors

--- lib/ReseqTrack/WebsiteUpdater/Controller/WebsiteUpdater.pm
+++ lib/ReseqTrack/WebsiteUpdater/Controller/WebsiteUpdater.pm
@@ -5,7 +5,6 @@ use Mojo::Util qw(slurp spurt);
 use EnsEMBL::Git;
 use File::Rsync;
 use File::Path;
-use JSON qw();
 use ReseqTrack::WebsiteUpdater::Model::PubSubHubBub;
 
 sub update_project {
@@ -14,11 +13,6 @@ sub update_project {
   my $project = $self->stash('project');
   my $project_config = $self->config('projects')->{$project};
   return $self->render(text=>"project $project does not exist\n", status=>404) if !$project_config;
-
-  my $req_json;
-  eval { $req_json = JSON::decode_json($self->req->body); };
-  return $self->render(text => 'error encoutered while parsing JSON', status => 400) if $@;
-  return $self->render(text=>"not parsing failed builds\n") if $req_json->{'error'};
 
   my $log_dir = $self->config('updating_log_dir') || $self->app->home->rel_dir('var/run');
   File::Path::make_path($log_dir);
