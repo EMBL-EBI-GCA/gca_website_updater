c071ddc2a80bb44a53933dff2d03eb1b28dc941b
Peter Harrison <peter@ebi.ac.uk>
2016-11-08 16:30:35 +0000

    Add feedburner ping

--- lib/ReseqTrack/WebsiteUpdater/Model/PubSubHubBub.pm
+++ lib/ReseqTrack/WebsiteUpdater/Model/PubSubHubBub.pm
@@ -25,6 +25,15 @@ sub publish {
   my $response = HTTP::Tiny->new->post_form($hub_href, {'hub.mode'=> 'publish', 'hub.url' => $rss_href});
   die "post to $hub_href failed: ".$response->{status}.$response->{content} if  $response->{status} !~ /^2\d\d$/;
 
+#Feedburner
+my $feedrss_href = $rss_href;
+$feedrss_href =~ s/rss\.xml//;
+#http://www.feedburner.com/fb/a/pingSubmit?bloglink=http://www.hipsci.org/announcements
+my $feedburner_href = 'http://www.feedburner.com/fb/a/pingSubmit?bloglink='.$feedrss_href;
+
+my $feedburner_response = HTTP::Tiny->new->post($feedburner_href);
+die "ping to $feedburner_href failed: ".$$feedburner_response->{status}.$$feedburner_response->{content} if  $$feedburner_response->{status} !~ /^2\d\d$/;
+
 }
 
 __PACKAGE__->meta->make_immutable;
